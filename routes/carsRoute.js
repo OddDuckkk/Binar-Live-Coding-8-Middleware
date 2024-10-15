74ee215c62e2eb470b8e1f748534d800a8089deb
Brandon OddDuckk <Brandon.Ogaden@gmail.com>
2024-10-15 18:44:19 +0800

    feat: imagekit credentials moved to env

--- routes/carsRoute.js
+++ routes/carsRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const upload = require('../middlewares/uploader');
+
 const router = express.Router();
 const carController = require("../controller/carController");
 
 // Cars API
-router.post("/", carController.createCar);
+router.post("/", upload.array('images'), carController.createCar);
 router.get("/", carController.getAllCars);
 router.get("/:id", carController.getCarById);
 router.delete("/:id", carController.deleteCarById);
